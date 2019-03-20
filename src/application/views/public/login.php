be36d18901b46bbc6ae3aaf8fc625db72922e07c
Noah Jahn <najahn19@gmail.com>
2019-03-20 08:57:25 -0400

    Started to add Auth controller and views.

---
+++ src/application/views/public/login.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <?php $this->load->view('require/head'); ?>
+
+    <?php $this->load->view('require/navbar'); ?>
+
+    <body>
+
+    </body>
+</html>
---
+++ src/application/views/require/head.php
@@ -0,0 +1,17 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/styles.css" />
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+
+    <!-- <link rel="stylesheet" href="<?php // echo base_url();?>assets/css/main.css" /> -->
+
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+    <!-- <script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script> -->
+
+    <title>CSM Group Inventory</title>
+
+</head>
