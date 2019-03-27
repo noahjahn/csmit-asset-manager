88548c8fa37ef49a7b2e97bb93c3ff579eca07e5
Noah Jahn <najahn19@gmail.com>
2019-03-27 19:33:37 -0400

    Started to setup login controller

--- src/application/controllers/Auth.php
+++ src/application/controllers/Auth.php
@@ -3,29 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		echo "Hello world!";
+	public function __construct() {
+		parent::__construct();
+
 	}
 
-	public function login()
-	{
-		$this->load->helper('url');
-		$this->load->view('public/login');
+	public function index() {
+
+	}
+
+	public function login()	{
+		$this->load->view('public/login/index');
 	}
 }
