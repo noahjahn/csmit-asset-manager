59cdfcae2c3e42c1612f9e49be5ed63074c9c25e
Noah Jahn <najahn19@gmail.com>
2019-03-27 19:38:58 -0400

    Moved and renamed login view

---
+++ src/application/views/public/login/index.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <?php $this->load->view('require/head'); ?>
+    <?php $this->load->view('require/navbar'); ?>
+
+    <body>
+        <div class="login_div">
+            <form class="login_form bg-light" action="#">
+
+            </form>
+        </div>
+    </body>
+</html>
