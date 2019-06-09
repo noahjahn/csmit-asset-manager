aae79e95c1fea2e1df8f76381eb9b541ae8e222f
Noah Jahn <najahn19@gmail.com>
2019-06-08 21:05:56 -0400

    Added automatic setup of docker enviornment variables

--- src/docker-init.sh
+++ src/docker-init.sh
@@ -1,5 +1,5 @@
 # script to automatically setup the docker environment for this project.
 # You're assumed to have Docker Engine release 18.06.0+
-# cp .env.docker .env
+cp .env.docker .env -f
 docker-compose down
 docker-compose up -d
