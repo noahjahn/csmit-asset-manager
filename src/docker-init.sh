bc2dd63481ecb08ded7c3f1afebdbe35ae22245d
Noah Jahn <najahn19@gmail.com>
2019-06-08 20:18:28 -0400

    Setup Docker images and compose system

---
+++ src/docker-init.sh
@@ -0,0 +1,5 @@
+# script to automatically setup the docker environment for this project.
+# You're assumed to have Docker Engine release 18.06.0+
+# cp .env.docker .env
+docker-compose down
+docker-compose up -d
