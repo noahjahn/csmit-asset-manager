7c38fb54b4f187c4aac325c23ddc6ce329bedef0
Noah Jahn <najahn19@gmail.com>
2019-06-09 14:47:02 -0400

    Setup initial view for login page with login page photo

--- src/application/views/public/login/index.php
+++ src/application/views/public/login/index.php
@@ -3,13 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
     <?php $this->load->view('require/head'); ?>
-    <?php $this->load->view('require/navbar'); ?>
-
     <body>
-        <div class="login_div">
-            <form class="login_form bg-light" action="#">
+        <div class="row">
+            <div class="col-lg-8 vh-100">
+                <img src="<?php echo $login_photo; ?>" alt="Login Photo" style="width: 100%; height: 100%; object-fit: cover;">
+            </div>
+            <div class="col-lg-4 align-self-center">
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8 text-left">
 
-            </form>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
