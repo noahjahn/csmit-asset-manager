a38621023581afc67ef74960c17236852c8692d5
Noah Jahn <najahn19@gmail.com>
2019-06-10 22:26:46 -0400

    Built out login form

--- src/application/views/public/login/index.php
+++ src/application/views/public/login/index.php
@@ -2,7 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
-    <?php $this->load->view('require/head'); ?>
+    <?php
+    $this->load->view('require/head');
+    $this->load->view('public/login/header');
+    ?>
     <body>
         <div class="row">
             <div class="col-lg-8 vh-100">
@@ -12,7 +15,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <div class="row">
                     <div class="col-sm-2"></div>
                     <div class="col-sm-8 text-left">
-
+                        <?php //add_style_to_error() ?>
+                        <h4>CSM Group Information Technology</h4>
+                        <?php echo form_open(current_url(), array( 'id' => 'login_form', 'name' => 'login_form')); ?>
+                            <fieldset>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" name="login_email" autocomplete="email" class="form-control" placeholder="email@example.com" value="<?php set_value('login_email'); ?>">
+                                    <div class="text-danger"><?php echo form_error("login_email"); ?></div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" name="login_password" autocomplete="password" class="form-control" placeholder="********" value="<?php set_value('login_password'); ?>">
+                                    <div class="text-danger"><?php echo form_error("login_password"); ?></div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" name="login_remember" id="login_remember" value="<?php //set_value('remember') ?>">
+                                        <label class="custom-control-label" for="login_remember">Remember me?</label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" name="login-submit" value="Login" class="form-control btn btn-primary">
+                                </div>
+                                <?php $this->session->flashdata('error'); ?>
+                            </fieldset>
+                        <?php echo form_close(); ?>
                     </div>
                     <div class="col-sm-2"></div>
                 </div>
