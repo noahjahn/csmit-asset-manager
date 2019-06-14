a84035cec428a72b50cf52d8566591ad6bd63797
Noah Jahn <najahn19@gmail.com>
2019-06-13 22:21:52 -0400

    Added javascript to login form to stop resubmit prompt on refresh

--- src/application/views/public/login/header.php
+++ src/application/views/public/login/header.php
@@ -1,4 +1,10 @@
 <head>
     <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/auth.js"></script>
     <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.validate.js"></script>
+    <script>
+        // stops the form resubmission prompt
+        if ( window.history.replaceState ) {
+            window.history.replaceState( null, null, window.location.href );
+        }
+    </script>
 </head>
