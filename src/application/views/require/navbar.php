0e7e823edc50b980bc4b010be7a28e17a37ec6c0
Noah Jahn <najahn19@gmail.com>
2019-06-15 11:51:55 -0400

    Updated navbar

--- src/application/views/require/navbar.php
+++ src/application/views/require/navbar.php
@@ -1,4 +1,4 @@
-<div class="bg-light border-right" id="sidebar-wrapper">
+<!-- <div class="bg-light border-right" id="sidebar-wrapper">
   <div class="sidebar-heading">CSM Group </div>
   <div class="list-group list-group-flush">
     <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
@@ -7,6 +7,156 @@
     <a href="#" class="list-group-item list-group-item-action bg-light">Inventories</a>
     <a href="#" class="list-group-item list-group-item-action bg-light">Reports</a>
     <a href="#" class="list-group-item list-group-item-action bg-light">Password Manager</a>
-    <a href="<?php echo base_url('logout'); ?>" class="list-group-item list-group-item-action bg-light">Logout</a>
+    <a href="<?php //echo base_url('settings'); ?>" class="list-group-item list-group-item-action bg-light">Settings</a>
+    <a href="<?php //echo base_url('logout'); ?>" class="list-group-item list-group-item-action bg-light">Logout</a>
   </div>
+</div> -->
+
+<!-- <div class="container-fluid mw-25">
+    <div class="row">
+        <div class="">
+            <nav class="navbar navbar-expand-md navbar-light bg-light px-0 flex-row flex-nowrap">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarWEX" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                 <div class="navbar-collapse collapse" id="navbarWEX">
+                    <div class="flex-sm-column flex-row">
+                        <ul class="nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Dashboard</a>
+                            </li>
+                        </ul>
+                        <a class="nav-item nav-link active" href="#">Dashboard</a>
+                        <a class="nav-item nav-link" href="#">Asset Manager</a>
+                        <a class="nav-item nav-link" href="#">Helpdesk</a>
+                        <a class="nav-item nav-link" href="#">Inventories</a>
+                        <a class="nav-item nav-link" href="#">Reports</a>
+                        <a class="nav-item nav-link" href="#">Password Manager</a>
+                        <a class="nav-item nav-link" href="<?php echo base_url('settings'); ?>">Settings</a>
+                        <a class="nav-item nav-link" href="<?php echo base_url('logout'); ?>">Logout</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div> -->
+
+<!-- <div class="container-fluid mw-25">
+<div class="row">
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-0 flex-row flex-nowrap">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor03">
+      <div class="flex-sm-column flex-row">
+        <ul class="nav mr-auto">
+        <a class="navbar-brand" href="#">CSM Group</a>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+    </div>
+  </div>
+</nav>
+</div>
+</div> -->
+<div class="container-fluid bg-light ml-0 mw-15 min-vh-100 pl-0">
+    <ul class="nav navbar-light">
+        <div class="navbar-nav w-100">
+            <div class="text-center">
+                <img src="<?php echo base_url(); ?>assets/img/male-circle-512.png" style="background-color: grey; max-width: 160px; border-radius: 50%">
+                <h4>Noah Jahn</h4>
+            </div>
+            <div style="border-left-style: solid; border-color: #3498DB;">
+                <li class="nav-item pl-3">
+                    <a class="nav-link active" href="#">
+                        <span>
+                            <img src="<?php echo base_url(); ?>assets/img/gear-svgrepo-com.svg" style="max-width: 32px;">
+                        </span>
+                        <span class="pl-3">Dashboard</span>
+                    </a>
+                </li>
+            </div>
+            <div style="">
+                <li class="nav-item pl-3 ml-3px">
+                    <a class="nav-link" href="#">
+                        <span>
+                            <img src="<?php echo base_url(); ?>assets/img/gear-svgrepo-com.svg" style="max-width: 32px;">
+                        </span>
+                        <span class="pl-3">Asset Manager</span>
+                    </a>
+                </li>
+            </div>
+            <div style="">
+                <li class="nav-item pl-3 ml-3px">
+                    <a class="nav-link" href="#">
+                        <span>
+                            <img src="<?php echo base_url(); ?>assets/img/gear-svgrepo-com.svg" style="max-width: 32px;">
+                        </span>
+                        <span class="pl-3">Helpdesk</span>
+                    </a>
+                </li>
+            </div>
+            <div style="">
+                <li class="nav-item pl-3 ml-3px">
+                    <a class="nav-link" href="#">
+                        <span>
+                            <img src="<?php echo base_url(); ?>assets/img/gear-svgrepo-com.svg" style="max-width: 32px;">
+                        </span>
+                        <span class="pl-3">Inventories</span>
+                    </a>
+                </li>
+            </div>
+            <div style="">
+                <li class="nav-item pl-3 ml-3px">
+                    <a class="nav-link" href="#">
+                        <span>
+                            <img src="<?php echo base_url(); ?>assets/img/gear-svgrepo-com.svg" style="max-width: 32px;">
+                        </span>
+                        <span class="pl-3">Reports</span>
+                    </a>
+                </li>
+            </div>
+            <div style="">
+                <li class="nav-item pl-3 ml-3px">
+                    <a class="nav-link" href="#">
+                        <span>
+                            <img src="<?php echo base_url(); ?>assets/img/gear-svgrepo-com.svg" style="max-width: 32px;">
+                        </span>
+                        <span class="pl-3">Password Manager</span>
+                    </a>
+                </li>
+            </div>
+            <div style="">
+                <li class="nav-item pl-3 ml-3px">
+                    <a class="nav-link" href="#">
+                        <span>
+                            <img src="<?php echo base_url(); ?>assets/img/gear-svgrepo-com.svg" style="max-width: 32px;">
+                        </span>
+                        <span class="pl-3">Settings</span>
+                    </a>
+                </li>
+            </div>
+            <div style="">
+                <li class="nav-item pl-3 ml-3px">
+                    <a class="nav-link" href="#">
+                        <span>
+                            <img src="<?php echo base_url(); ?>assets/img/gear-svgrepo-com.svg" style="max-width: 32px;">
+                        </span>
+                        <span class="pl-3">Logout</span>
+                    </a>
+                </li>
+            </div>
+        </div>
+    </ul>
 </div>
