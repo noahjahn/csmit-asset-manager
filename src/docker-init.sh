55ea07f33e97a5ef9da38701d63394220c4629f0
Noah Jahn <najahn19@gmail.com>
2019-06-18 21:28:41 -0400

    Reordered command arguments

--- src/docker-init.sh
+++ src/docker-init.sh
@@ -1,5 +1,10 @@
-# script to automatically setup the docker environment for this project.
+# Script to automatically setup the docker environment for this project.
 # You're assumed to have Docker Engine release 18.06.0+
-cp .env.docker .env -f
-docker-compose down
-docker-compose up -d
+docker-compose down # stop any currently running containers
+docker rm $(docker ps -aq) -f # remove all containers
+# docker-compose build # build if there are any changes
+cp -f .env.docker .env # setup local environment file
+rm -rf application/vendor/* # remove vendors folder so the dependencies are installed each time
+docker system prune -f
+docker volume prune -f
+docker-compose up -d # start the docker containers and connect using the docker-compose.yml
