c8ec69919fbed5f5e41e3f3b31ab8019dcb83871
Noah Jahn <najahn19@gmail.com>
2019-08-04 11:31:35 -0400

    Added settings javascript

---
+++ src/assets/js/settings.js
@@ -0,0 +1,124 @@
+$(function(){
+    $('#asset-manager-link').click(function(e) {
+        e.preventDefault();
+        $(this).addClass('active')
+        $('#users-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#permissions-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#login-photos-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#asset-manager-content').each(function() {
+            $(this).addClass('show');
+            $(this).addClass('active');
+        });
+        $('#users-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+        $('#permissions-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+        $('#login-photos-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+    });
+});
+
+$(function(){
+    $('#users-link').click(function() {
+        $(this).addClass('active')
+        $('#asset-manager-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#permissions-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#login-photos-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#users-content').each(function() {
+            $(this).addClass('show');
+            $(this).addClass('active');
+        });
+        $('#asset-manager-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+        $('#permissions-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+        $('#login-photos-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+    });
+});
+
+$(function(){
+    $('#permissions-link').click(function() {
+        $(this).addClass('active')
+        $('#users-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#asset-manager-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#login-photos-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#permissions-content').each(function() {
+            $(this).addClass('show');
+            $(this).addClass('active');
+        });
+        $('#users-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+        $('#asset-manager-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+        $('#login-photos-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+    });
+});
+
+$(function(){
+    $('#login-photos-link').click(function() {
+        $(this).addClass('active')
+        $('#users-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#permissions-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#asset-manager-link').each(function() {
+            $(this).removeClass('active');
+        });
+        $('#login-photos-content').each(function() {
+            $(this).addClass('show');
+            $(this).addClass('active');
+        });
+        $('#users-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+        $('#permissions-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+        $('#asset-manager-content').each(function() {
+            $(this).removeClass('show');
+            $(this).removeClass('active');
+        });
+    });
+});
