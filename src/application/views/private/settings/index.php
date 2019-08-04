1fc3787f60f34b314d34c3594dbc73a6b9dd19ca
Noah Jahn <najahn19@gmail.com>
2019-08-04 11:32:43 -0400

    Setup settings index page

--- src/application/views/private/settings/index.php
+++ src/application/views/private/settings/index.php
@@ -1,136 +1,23 @@
+<script src="<?php echo base_url(); ?>assets/js/settings.js"></script>
 <div class="row">
     <ul class="nav nav-tabs" style="width: 100%">
         <li class="nav-item">
-            <a class="nav-link active" href="#">Asset Manager</a>
+            <a id="asset-manager-link" class="nav-link active" href="#asset_manager">Asset Manager</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Users</a>
+            <a id="users-link" class="nav-link" href="#users">Users</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link disabled" href="#">Permissions</a>
+            <a id="permissions-link" class="nav-link disabled" href="#permissions">Permissions</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Login Photos</a>
+            <a id="login-photos-link" class="nav-link" href="#login_photos">Login Photos</a>
         </li>
     </ul>
-    <div class="container-fluid container-style">
-        <div class="row">
-            <div class="col">
-                <div class="container pt-2">
-                    <h4>Asset Types</h4>
-                    <div class="container inner-quarter-container">
-                        <div class="quarter-table">
-                            <table class="table table-hover">
-                                <col width="60%">
-                                <col width="20%">
-                                <col width="10%">
-                                <col width="10%">
-                                <thead>
-                                    <tr class="table-primary">
-                                        <th>Name</th>
-                                        <th>Rate</th>
-                                        <th></th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody class="">
-                                    <tr>
-                                        <td>Computers</td>
-                                        <td>$100.00</td>
-                                        <td><img class="mini-icon" src="<?php echo base_url(); ?>assets/img/icons/edit-svgrepo-com.svg"></td>
-                                        <td><img class="mini-icon" src="<?php echo base_url(); ?>assets/img/icons/trash-can-with-cover-svgrepo-com.svg"></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <button type="button" class="btn btn-primary">Add Asset Type</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="container pt-2">
-                    <h4>Teams</h4>
-                    <div class="container inner-quarter-container">
-                        <table class="table table-hover" style="table-layout: auto;">
-                            <thead>
-                                <tr class="table-primary">
-                                    <!-- <th scope="col">Name</th> -->
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Rate</th>
-                                    <th scope="col"></th>
-                                    <th scope="col"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Computers</td>
-                                    <td>$100.00</td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <button>Add Team</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <div class="container pt-2">
-                    <h4>Manufacturers</h4>
-                    <div class="container inner-quarter-container">
-                        <table class="table table-hover" style="table-layout: auto;">
-
-                            <thead>
-                                <tr class="table-primary">
-                                    <!-- <th scope="col">Name</th> -->
-                                    <th>Name</th>
-                                    <th>Rate</th>
-                                    <th>t</th>
-                                    <th>t</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Computers</td>
-                                    <td>$100.00</td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <button>Add Manufacturer</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="container pt-2">
-                    <h4>Models</h4>
-                    <div class="container inner-quarter-container">
-                        <table class="table table-hover" style="table-layout: auto;">
-                            <thead>
-                                <tr class="table-primary">
-                                    <!-- <th scope="col">Name</th> -->
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Rate</th>
-                                    <th scope="col"></th>
-                                    <th scope="col"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Computers</td>
-                                    <td>$100.00</td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <button>Add Model</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="tab-content" id="nav-tabContent">
+        <div id="asset-manager-content" class="tab-pane fade" role="tabpanel"><?php $this->load->view('private/settings/asset_manager');?></div>
+        <div id="users-content" class="tab-pane fade" role="tabpanel"><?php $this->load->view('private/settings/users', $this->data);?></div>
+        <div id="permissions-content" class="tab-pane fade" role="tabpanel"><?php $this->load->view('private/settings/permissions', $this->data);?></div>
+        <div id="login-photos-content" class="tab-pane fade" role="tabpanel"><?php $this->load->view('private/settings/login_photos', $this->data);?></div>
     </div>
 </div>
