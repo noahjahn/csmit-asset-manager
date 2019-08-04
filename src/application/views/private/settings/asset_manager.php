dcf37d136d6a3d8466af91cf5b05a7c53341009b
Noah Jahn <najahn19@gmail.com>
2019-08-04 11:31:17 -0400

    Added single page views for the settings sections

---
+++ src/application/views/private/settings/asset_manager.php
@@ -0,0 +1,120 @@
+<div class="container-fluid container-style">
+    <div class="row">
+        <div class="col">
+            <div class="container pt-2">
+                <h4>Asset Types</h4>
+                <div class="container inner-quarter-container">
+                    <div class="quarter-table">
+                        <table class="table table-hover table-sm">
+                            <col width="60%">
+                            <col width="20%">
+                            <col width="10%">
+                            <col width="10%">
+                            <thead>
+                                <tr class="table-primary">
+                                    <th>Name</th>
+                                    <th>Rate</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody class="">
+                                <tr>
+                                    <td>Computers</td>
+                                    <td>$100.00</td>
+                                    <td><span class="table-icon"><img class="mini-icon" src="<?php echo base_url(); ?>assets/img/icons/edit-svgrepo-com-white.svg"><span></td>
+                                    <td><img class="mini-icon" src="<?php echo base_url(); ?>assets/img/icons/trash-can-with-cover-svgrepo-com-white.svg"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <button type="button" class="btn btn-primary">Add Asset Type</button>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="container pt-2">
+                <h4>Teams</h4>
+                <div class="container inner-quarter-container">
+                    <table class="table table-hover" style="table-layout: auto;">
+                        <thead>
+                            <tr class="table-primary">
+                                <!-- <th scope="col">Name</th> -->
+                                <th scope="col">Name</th>
+                                <th scope="col">Rate</th>
+                                <th scope="col"></th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Computers</td>
+                                <td>$100.00</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button>Add Team</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="container pt-2">
+                <h4>Manufacturers</h4>
+                <div class="container inner-quarter-container">
+                    <table class="table table-hover" style="table-layout: auto;">
+
+                        <thead>
+                            <tr class="table-primary">
+                                <!-- <th scope="col">Name</th> -->
+                                <th>Name</th>
+                                <th>Rate</th>
+                                <th>t</th>
+                                <th>t</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Computers</td>
+                                <td>$100.00</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button>Add Manufacturer</button>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="container pt-2">
+                <h4>Models</h4>
+                <div class="container inner-quarter-container">
+                    <table class="table table-hover" style="table-layout: auto;">
+                        <thead>
+                            <tr class="table-primary">
+                                <!-- <th scope="col">Name</th> -->
+                                <th scope="col">Name</th>
+                                <th scope="col">Rate</th>
+                                <th scope="col"></th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Computers</td>
+                                <td>$100.00</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button>Add Model</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
