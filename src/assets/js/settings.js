78185735aede2d0572eb69c76abc5fab7edd215b
Noah Jahn <najahn19@gmail.com>
2019-08-08 22:28:14 -0400

    Added asset_manager on load

--- src/assets/js/settings.js
+++ src/assets/js/settings.js
@@ -1,32 +1,41 @@
+$(document).ready(function(){
+    console.log("test");
+    makeAssetManagerActive();
+});
+
+function makeAssetManagerActive() {
+    $(this).addClass('active')
+    $('#users-link').each(function() {
+        $(this).removeClass('active');
+    });
+    $('#permissions-link').each(function() {
+        $(this).removeClass('active');
+    });
+    $('#login-photos-link').each(function() {
+        $(this).removeClass('active');
+    });
+    $('#asset-manager-content').each(function() {
+        $(this).addClass('show');
+        $(this).addClass('active');
+    });
+    $('#users-content').each(function() {
+        $(this).removeClass('show');
+        $(this).removeClass('active');
+    });
+    $('#permissions-content').each(function() {
+        $(this).removeClass('show');
+        $(this).removeClass('active');
+    });
+    $('#login-photos-content').each(function() {
+        $(this).removeClass('show');
+        $(this).removeClass('active');
+    });
+}
+
 $(function(){
     $('#asset-manager-link').click(function(e) {
         e.preventDefault();
-        $(this).addClass('active')
-        $('#users-link').each(function() {
-            $(this).removeClass('active');
-        });
-        $('#permissions-link').each(function() {
-            $(this).removeClass('active');
-        });
-        $('#login-photos-link').each(function() {
-            $(this).removeClass('active');
-        });
-        $('#asset-manager-content').each(function() {
-            $(this).addClass('show');
-            $(this).addClass('active');
-        });
-        $('#users-content').each(function() {
-            $(this).removeClass('show');
-            $(this).removeClass('active');
-        });
-        $('#permissions-content').each(function() {
-            $(this).removeClass('show');
-            $(this).removeClass('active');
-        });
-        $('#login-photos-content').each(function() {
-            $(this).removeClass('show');
-            $(this).removeClass('active');
-        });
+        makeAssetManagerActive();
     });
 });
 
