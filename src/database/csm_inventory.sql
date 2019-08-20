7cbe536188b29e345eab92d8ba28fee20fe0db7a
Noah Jahn <najahn19@gmail.com>
2019-08-19 22:39:23 -0400

    Added latest database dump

--- src/database/csm_inventory.sql
+++ src/database/csm_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Jun 13, 2019 at 03:38 AM
+-- Generation Time: Aug 20, 2019 at 02:38 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.19
 
@@ -28,36 +28,39 @@ SET time_zone = "+00:00";
 -- Table structure for table `assets`
 --
 
-DROP TABLE IF EXISTS `assets`;
 CREATE TABLE `assets` (
   `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `manufacturer` int(11) NOT NULL,
   `model` int(11) NOT NULL,
-  `owner` varchar(128) NOT NULL,
-  `serial_number` varchar(128) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `serial_number` varchar(255) NOT NULL,
   `type` int(11) NOT NULL,
   `asset_tag` int(11) NOT NULL,
   `purchase_price` decimal(13,2) DEFAULT NULL,
   `purchase_date` date DEFAULT NULL,
-  `location` varchar(128) NOT NULL,
+  `location` varchar(255) NOT NULL,
   `team` int(11) NOT NULL,
   `job_number` int(11) DEFAULT NULL,
-  `last_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
+  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(255) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `assets`
 --
 
-INSERT INTO `assets` (`id`, `name`, `manufacturer`, `model`, `owner`, `serial_number`, `type`, `asset_tag`, `purchase_price`, `purchase_date`, `location`, `team`, `job_number`, `last_updated`) VALUES
-(1, '1', 1, 1, 'IT Department', '2WZRNF2', 1, 3349, '1574.60', '2019-03-21', 'Kalamazoo', 1, NULL, '2019-03-26 19:30:24'),
-(2, '2', 2, 2, 'Noah Jahn', 'PEWPD6JA00946', 2, 1373, NULL, NULL, 'Kalamazoo', 1, NULL, '2019-03-27 16:38:15'),
-(3, '2', 2, 2, 'Noah Jahn', 'PEWPD8JA00341', 2, 2622, NULL, NULL, 'Kalamazoo', 1, NULL, '2019-03-27 16:38:15'),
-(4, '3', 2, 3, 'Noah Jahn', 'PL751NSS03674', 2, 2977, NULL, NULL, 'Kalamazoo', 2, NULL, '2019-03-27 16:41:32'),
-(5, '3', 2, 3, 'Noah Jahn', 'PL751NSS03465', 2, 2935, NULL, NULL, 'Kalamazoo', 2, NULL, '2019-03-27 16:41:32'),
-(6, '4', 3, 4, 'Noah Jahn', '171115234500327', 1, 3100, NULL, NULL, 'Kalamazoo', 1, NULL, '2019-03-27 16:43:10'),
-(7, '5', 1, 5, 'Noah Jahn', 'auto-1550095609.740821-1bc35', 3, 6548, NULL, NULL, 'Kalamazoo', 2, NULL, '2019-03-27 16:43:10');
+INSERT INTO `assets` (`id`, `name`, `manufacturer`, `model`, `owner`, `serial_number`, `type`, `asset_tag`, `purchase_price`, `purchase_date`, `location`, `team`, `job_number`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, '1', 1, 1, 'IT Department', '2WZRNF2', 1, 3349, '1574.60', '2019-03-21', 'Kalamazoo', 1, NULL, 1, '', '2019-08-20 01:21:35', '', '2019-08-20 01:21:35'),
+(2, '2', 2, 2, 'Noah Jahn', 'PEWPD6JA00946', 2, 1373, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-20 01:21:35', '', '2019-08-20 01:21:35'),
+(3, '2', 2, 2, 'Noah Jahn', 'PEWPD8JA00341', 2, 2622, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-20 01:21:35', '', '2019-08-20 01:21:35'),
+(4, '3', 2, 3, 'Noah Jahn', 'PL751NSS03674', 2, 2977, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-20 01:21:35', '', '2019-08-20 01:21:35'),
+(5, '3', 2, 3, 'Noah Jahn', 'PL751NSS03465', 2, 2935, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-20 01:21:35', '', '2019-08-20 01:21:35'),
+(6, '4', 3, 4, 'Noah Jahn', '171115234500327', 1, 3100, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-20 01:21:35', '', '2019-08-20 01:21:35'),
+(7, '5', 1, 5, 'Noah Jahn', 'auto-1550095609.740821-1bc35', 3, 6548, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-20 01:21:35', '', '2019-08-20 01:21:35');
 
 -- --------------------------------------------------------
 
@@ -65,20 +68,24 @@ INSERT INTO `assets` (`id`, `name`, `manufacturer`, `model`, `owner`, `serial_nu
 -- Table structure for table `asset_types`
 --
 
-DROP TABLE IF EXISTS `asset_types`;
 CREATE TABLE `asset_types` (
   `id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
-  `rate` decimal(13,2) NOT NULL
+  `rate` decimal(13,2) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
+  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(255) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `asset_types`
 --
 
-INSERT INTO `asset_types` (`id`, `name`, `rate`) VALUES
-(1, 'Computers', '100.00'),
-(2, 'Monitors', '10.00');
+INSERT INTO `asset_types` (`id`, `name`, `rate`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'Computers', '100.00', 1, '', '2019-08-20 01:21:45', '', '2019-08-20 01:21:45'),
+(2, 'Monitors', '10.00', 1, '', '2019-08-20 01:21:45', '', '2019-08-20 01:21:45');
 
 -- --------------------------------------------------------
 
@@ -86,23 +93,25 @@ INSERT INTO `asset_types` (`id`, `name`, `rate`) VALUES
 -- Table structure for table `login_photos`
 --
 
-DROP TABLE IF EXISTS `login_photos`;
 CREATE TABLE `login_photos` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `path` varchar(255) NOT NULL,
-  `uploaded_by` varchar(255) NOT NULL,
-  `date_uploaded` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
+  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(255) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login_photos`
 --
 
-INSERT INTO `login_photos` (`id`, `name`, `path`, `uploaded_by`, `date_uploaded`) VALUES
-(1, 'images.jpg', 'images.jpg', 'administrator@csmgroup.com', '2019-06-10 12:11:59'),
-(2, 'information-technology.jpg', 'information-technology.jpg', 'administrator@csmgroup.com', '2019-06-10 12:11:59'),
-(3, 'pexels-photo-546819.jpeg', 'pexels-photo-546819.jpeg', 'administrator@csmgroup.com', '2019-06-10 12:12:23');
+INSERT INTO `login_photos` (`id`, `name`, `path`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'images.jpg', 'images.jpg', 1, '', '2019-08-20 01:21:55', '', '2019-08-20 01:21:55'),
+(2, 'information-technology.jpg', 'information-technology.jpg', 1, '', '2019-08-20 01:21:55', '', '2019-08-20 01:21:55'),
+(3, 'pexels-photo-546819.jpeg', 'pexels-photo-546819.jpeg', 1, '', '2019-08-20 01:21:55', '', '2019-08-20 01:21:55');
 
 -- --------------------------------------------------------
 
@@ -110,20 +119,24 @@ INSERT INTO `login_photos` (`id`, `name`, `path`, `uploaded_by`, `date_uploaded`
 -- Table structure for table `manufacturers`
 --
 
-DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
   `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL
+  `name` varchar(128) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
+  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(255) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `manufacturers`
 --
 
-INSERT INTO `manufacturers` (`id`, `name`) VALUES
-(1, 'Dell'),
-(2, 'Planar'),
-(3, 'ASUS');
+INSERT INTO `manufacturers` (`id`, `name`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'Dell', 1, '', '2019-08-20 01:22:05', '', '2019-08-20 01:22:05'),
+(2, 'Planar', 1, '', '2019-08-20 01:22:05', '', '2019-08-20 01:22:05'),
+(3, 'ASUS', 1, '', '2019-08-20 01:22:05', '', '2019-08-20 01:22:05');
 
 -- --------------------------------------------------------
 
@@ -131,23 +144,27 @@ INSERT INTO `manufacturers` (`id`, `name`) VALUES
 -- Table structure for table `models`
 --
 
-DROP TABLE IF EXISTS `models`;
 CREATE TABLE `models` (
   `id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
-  `manufacturer` int(11) NOT NULL
+  `manufacturer` int(11) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
+  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(255) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `models`
 --
 
-INSERT INTO `models` (`id`, `name`, `manufacturer`) VALUES
-(1, 'XPS 15', 1),
-(2, 'PXL2451MW', 2),
-(3, 'PXL2470MW', 2),
-(4, 'Strix-I Gaming', 3),
-(5, 'WD15', 1);
+INSERT INTO `models` (`id`, `name`, `manufacturer`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'XPS 15', 1, 1, '', '2019-08-20 01:22:13', '', '2019-08-20 01:22:13'),
+(2, 'PXL2451MW', 2, 1, '', '2019-08-20 01:22:13', '', '2019-08-20 01:22:13'),
+(3, 'PXL2470MW', 2, 1, '', '2019-08-20 01:22:13', '', '2019-08-20 01:22:13'),
+(4, 'Strix-I Gaming', 3, 1, '', '2019-08-20 01:22:13', '', '2019-08-20 01:22:13'),
+(5, 'WD15', 1, 1, '', '2019-08-20 01:22:13', '', '2019-08-20 01:22:13');
 
 -- --------------------------------------------------------
 
@@ -155,19 +172,23 @@ INSERT INTO `models` (`id`, `name`, `manufacturer`) VALUES
 -- Table structure for table `teams`
 --
 
-DROP TABLE IF EXISTS `teams`;
 CREATE TABLE `teams` (
   `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL
+  `name` varchar(128) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
+  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(255) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `teams`
 --
 
-INSERT INTO `teams` (`id`, `name`) VALUES
-(1, 'Zulu'),
-(2, 'SPARK');
+INSERT INTO `teams` (`id`, `name`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'Zulu', 1, '', '2019-08-20 01:22:18', '', '2019-08-20 01:22:18'),
+(2, 'SPARK', 1, '', '2019-08-20 01:22:18', '', '2019-08-20 01:22:18');
 
 -- --------------------------------------------------------
 
@@ -175,24 +196,27 @@ INSERT INTO `teams` (`id`, `name`) VALUES
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `first_name` varchar(255) COLLATE utf8_bin NOT NULL,
   `last_name` varchar(255) COLLATE utf8_bin NOT NULL,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `session_token` varchar(255) COLLATE utf8_bin NOT NULL,
   `last_login` datetime DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
-  `updated_date` datetime DEFAULT NULL
+  `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
+  `last_modified_by` varchar(255) COLLATE utf8_bin NOT NULL,
+  `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(255) COLLATE utf8_bin NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `last_login`, `created_date`, `updated_date`) VALUES
-(1, 'Super', 'User', 'administrator@csmgroup.com', '$2y$10$e9LFqB0jKJnQnkwxlKWWyuaCU6pA/R0rcyO7mnSPXdqWJV5SHiB0K', NULL, '2019-06-10 22:05:10', NULL);
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `session_token`, `last_login`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'Super', 'User', 'administrator@csmgroup.com', '$2y$10$e9LFqB0jKJnQnkwxlKWWyuaCU6pA/R0rcyO7mnSPXdqWJV5SHiB0K', '$2y$10$JtUS.IFK9IpRVDy/73F81eXE0eW9cCBSNeJbCwqdSQJIrQ9nESnW6', '2019-08-20 01:43:44', 1, '', '2019-08-20 01:21:16', '', '2019-08-20 01:21:16');
 
 --
 -- Indexes for dumped tables
@@ -233,7 +257,8 @@ ALTER TABLE `manufacturers`
 -- Indexes for table `models`
 --
 ALTER TABLE `models`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `manufacturer` (`manufacturer`);
 
 --
 -- Indexes for table `teams`
@@ -309,6 +334,12 @@ ALTER TABLE `assets`
 --
 ALTER TABLE `asset_types`
   ADD CONSTRAINT `asset_types_ibfk_1` FOREIGN KEY (`id`) REFERENCES `assets` (`type`);
+
+--
+-- Constraints for table `models`
+--
+ALTER TABLE `models`
+  ADD CONSTRAINT `manufacturer` FOREIGN KEY (`manufacturer`) REFERENCES `manufacturers` (`id`);
 
 --
 -- Constraints for table `teams`
