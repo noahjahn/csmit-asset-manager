1e4d64a03c30f909324ccb82794bd0ab293b8001
Noah Jahn <najahn19@gmail.com>
2019-09-01 14:27:50 -0400

    Updated column names in the database

--- src/database/csm_inventory.sql
+++ src/database/csm_inventory.sql
@@ -43,38 +43,38 @@ CREATE TABLE `assets` (
   `team` int(11) NOT NULL,
   `job_number` int(11) DEFAULT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
-  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_by` int(11) NOT NULL,
   `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` varchar(255) NOT NULL,
-  `created_by_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_by` int(11) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `assets`
 --
 
-INSERT INTO `assets` (`id`, `name`, `manufacturer`, `model`, `owner`, `serial_number`, `type`, `asset_tag`, `purchase_price`, `purchase_date`, `location`, `team`, `job_number`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_by_time`) VALUES
-(1, '1', 1, 1, 'IT Department', '2WZRNF2', 1, 3349, '1574.60', '2019-03-21', 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(2, '2', 2, 2, 'Noah Jahn', 'PEWPD6JA00946', 2, 1373, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(3, '2', 2, 2, 'Noah Jahn', 'PEWPD8JA00341', 2, 2622, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(4, '3', 2, 3, 'Noah Jahn', 'PL751NSS03674', 2, 2977, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(5, '3', 2, 3, 'Noah Jahn', 'PL751NSS03465', 2, 2935, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(6, '4', 3, 4, 'Noah Jahn', '171115234500327', 1, 3100, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(7, '5', 1, 5, 'Noah Jahn', 'auto-1550095609.740821-1bc35', 3, 6548, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(8, '1', 1, 1, 'IT Department', '2WZRNF2', 1, 3349, '1574.60', '2019-03-21', 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(9, '2', 2, 2, 'Noah Jahn', 'PEWPD6JA00946', 2, 1373, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(10, '2', 2, 2, 'Noah Jahn', 'PEWPD8JA00341', 2, 2622, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(11, '3', 2, 3, 'Noah Jahn', 'PL751NSS03674', 2, 2977, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(12, '3', 2, 3, 'Noah Jahn', 'PL751NSS03465', 2, 2935, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(13, '4', 3, 4, 'Noah Jahn', '171115234500327', 1, 3100, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(14, '5', 1, 5, 'Noah Jahn', 'auto-1550095609.740821-1bc35', 3, 6548, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(15, '1', 1, 1, 'IT Department', '2WZRNF2', 1, 3349, '1574.60', '2019-03-21', 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(16, '2', 2, 2, 'Noah Jahn', 'PEWPD6JA00946', 2, 1373, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(17, '2', 2, 2, 'Noah Jahn', 'PEWPD8JA00341', 2, 2622, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(18, '3', 2, 3, 'Noah Jahn', 'PL751NSS03674', 2, 2977, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(19, '3', 2, 3, 'Noah Jahn', 'PL751NSS03465', 2, 2935, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(20, '4', 3, 4, 'Noah Jahn', '171115234500327', 1, 3100, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26'),
-(21, '5', 1, 5, 'Noah Jahn', 'auto-1550095609.740821-1bc35', 3, 6548, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '', '2019-08-19 02:21:26', '', '2019-08-19 02:21:26');
+INSERT INTO `assets` (`id`, `name`, `manufacturer`, `model`, `owner`, `serial_number`, `type`, `asset_tag`, `purchase_price`, `purchase_date`, `location`, `team`, `job_number`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, '1', 1, 1, 'IT Department', '2WZRNF2', 1, 3349, '1574.60', '2019-03-21', 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(2, '2', 2, 2, 'Noah Jahn', 'PEWPD6JA00946', 2, 1373, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(3, '2', 2, 2, 'Noah Jahn', 'PEWPD8JA00341', 2, 2622, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(4, '3', 2, 3, 'Noah Jahn', 'PL751NSS03674', 2, 2977, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(5, '3', 2, 3, 'Noah Jahn', 'PL751NSS03465', 2, 2935, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(6, '4', 3, 4, 'Noah Jahn', '171115234500327', 1, 3100, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(7, '5', 1, 5, 'Noah Jahn', 'auto-1550095609.740821-1bc35', 3, 6548, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(8, '1', 1, 1, 'IT Department', '2WZRNF2', 1, 3349, '1574.60', '2019-03-21', 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(9, '2', 2, 2, 'Noah Jahn', 'PEWPD6JA00946', 2, 1373, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(10, '2', 2, 2, 'Noah Jahn', 'PEWPD8JA00341', 2, 2622, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(11, '3', 2, 3, 'Noah Jahn', 'PL751NSS03674', 2, 2977, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(12, '3', 2, 3, 'Noah Jahn', 'PL751NSS03465', 2, 2935, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(13, '4', 3, 4, 'Noah Jahn', '171115234500327', 1, 3100, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(14, '5', 1, 5, 'Noah Jahn', 'auto-1550095609.740821-1bc35', 3, 6548, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(15, '1', 1, 1, 'IT Department', '2WZRNF2', 1, 3349, '1574.60', '2019-03-21', 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(16, '2', 2, 2, 'Noah Jahn', 'PEWPD6JA00946', 2, 1373, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(17, '2', 2, 2, 'Noah Jahn', 'PEWPD8JA00341', 2, 2622, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(18, '3', 2, 3, 'Noah Jahn', 'PL751NSS03674', 2, 2977, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(19, '3', 2, 3, 'Noah Jahn', 'PL751NSS03465', 2, 2935, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(20, '4', 3, 4, 'Noah Jahn', '171115234500327', 1, 3100, NULL, NULL, 'Kalamazoo', 1, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26'),
+(21, '5', 1, 5, 'Noah Jahn', 'auto-1550095609.740821-1bc35', 3, 6548, NULL, NULL, 'Kalamazoo', 2, NULL, 1, '1', '2019-08-19 02:21:26', '1', '2019-08-19 02:21:26');
 
 -- --------------------------------------------------------
 
@@ -87,19 +87,19 @@ CREATE TABLE `asset_types` (
   `name` varchar(128) NOT NULL,
   `rate` decimal(13,2) NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
-  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_by` int(11) NOT NULL,
   `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` varchar(255) NOT NULL,
-  `created_by_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_by` int(11) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `asset_types`
 --
 
-INSERT INTO `asset_types` (`id`, `name`, `rate`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_by_time`) VALUES
-(1, 'Computers', '100.00', 1, '', '2019-08-19 02:12:53', '', '2019-08-19 02:12:53'),
-(2, 'Monitors', '10.00', 1, '', '2019-08-19 02:12:53', '', '2019-08-19 02:12:53');
+INSERT INTO `asset_types` (`id`, `name`, `rate`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'Computers', '100.00', 1, '1', '2019-08-19 02:12:53', '1', '2019-08-19 02:12:53'),
+(2, 'Monitors', '10.00', 1, '1', '2019-08-19 02:12:53', '1', '2019-08-19 02:12:53');
 
 -- --------------------------------------------------------
 
@@ -111,18 +111,20 @@ CREATE TABLE `login_photos` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `path` varchar(255) NOT NULL,
-  `uploaded_by` varchar(255) NOT NULL,
-  `date_uploaded` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `last_modified_by` int(11) NOT NULL,
+  `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login_photos`
 --
 
-INSERT INTO `login_photos` (`id`, `name`, `path`, `uploaded_by`, `date_uploaded`) VALUES
-(1, 'images.jpg', 'images.jpg', 'administrator@csmgroup.com', '2019-06-10 12:11:59'),
-(2, 'information-technology.jpg', 'information-technology.jpg', 'administrator@csmgroup.com', '2019-06-10 12:11:59'),
-(3, 'pexels-photo-546819.jpeg', 'pexels-photo-546819.jpeg', 'administrator@csmgroup.com', '2019-06-10 12:12:23');
+INSERT INTO `login_photos` (`id`, `name`, `path`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'images.jpg', 'images.jpg', '1', '2019-06-10 12:11:59', '1', '2019-06-10 12:11:59'),
+(2, 'information-technology.jpg', 'information-technology.jpg', '1', '2019-06-10 12:11:59', '1', '2019-06-10 12:11:59'),
+(3, 'pexels-photo-546819.jpeg', 'pexels-photo-546819.jpeg', '1', '2019-06-10 12:12:23', '1', '2019-06-10 12:11:59');
 
 -- --------------------------------------------------------
 
@@ -134,20 +136,20 @@ CREATE TABLE `manufacturers` (
   `id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
-  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_by` int(11) NOT NULL,
   `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` varchar(255) NOT NULL,
-  `created_by_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_by` int(11) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `manufacturers`
 --
 
-INSERT INTO `manufacturers` (`id`, `name`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_by_time`) VALUES
-(1, 'Dell', 1, '', '2019-08-19 02:10:06', '', '2019-08-19 02:10:06'),
-(2, 'Planar', 1, '', '2019-08-19 02:10:06', '', '2019-08-19 02:10:06'),
-(3, 'ASUS', 1, '', '2019-08-19 02:10:06', '', '2019-08-19 02:10:06');
+INSERT INTO `manufacturers` (`id`, `name`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'Dell', 1, '1', '2019-08-19 02:10:06', '1', '2019-08-19 02:10:06'),
+(2, 'Planar', 1, '1', '2019-08-19 02:10:06', '1', '2019-08-19 02:10:06'),
+(3, 'ASUS', 1, '1', '2019-08-19 02:10:06', '1', '2019-08-19 02:10:06');
 
 -- --------------------------------------------------------
 
@@ -160,9 +162,9 @@ CREATE TABLE `models` (
   `name` varchar(128) NOT NULL,
   `manufacturer` int(11) NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
-  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_by` int(11) NOT NULL,
   `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` varchar(255) NOT NULL,
+  `created_by` int(11) NOT NULL,
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -171,11 +173,11 @@ CREATE TABLE `models` (
 --
 
 INSERT INTO `models` (`id`, `name`, `manufacturer`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
-(1, 'XPS 15', 1, 1, '', '2019-08-19 01:10:29', '', '2019-08-19 01:11:13'),
-(2, 'PXL2451MW', 2, 1, '', '2019-08-19 01:10:29', '', '2019-08-19 01:11:13'),
-(3, 'PXL2470MW', 2, 1, '', '2019-08-19 01:10:29', '', '2019-08-19 01:11:13'),
-(4, 'Strix-I Gaming', 3, 1, '', '2019-08-19 01:10:29', '', '2019-08-19 01:11:13'),
-(5, 'WD15', 1, 1, '', '2019-08-19 01:10:29', '', '2019-08-19 01:11:13');
+(1, 'XPS 15', 1, 1, '1', '2019-08-19 01:10:29', '1', '2019-08-19 01:11:13'),
+(2, 'PXL2451MW', 2, 1, '1', '2019-08-19 01:10:29', '1', '2019-08-19 01:11:13'),
+(3, 'PXL2470MW', 2, 1, '1', '2019-08-19 01:10:29', '1', '2019-08-19 01:11:13'),
+(4, 'Strix-I Gaming', 3, 1, '1', '2019-08-19 01:10:29', '1', '2019-08-19 01:11:13'),
+(5, 'WD15', 1, 1, '1', '2019-08-19 01:10:29', '1', '2019-08-19 01:11:13');
 
 -- --------------------------------------------------------
 
@@ -187,19 +189,19 @@ CREATE TABLE `teams` (
   `id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 active; 0 inactive',
-  `last_modified_by` varchar(255) NOT NULL,
+  `last_modified_by` int(11) NOT NULL,
   `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` varchar(255) NOT NULL,
-  `created_by_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_by` int(11) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `teams`
 --
 
-INSERT INTO `teams` (`id`, `name`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_by_time`) VALUES
-(1, 'Zulu', 1, '', '2019-08-19 02:12:40', '', '2019-08-19 02:12:40'),
-(2, 'SPARK', 1, '', '2019-08-19 02:12:40', '', '2019-08-19 02:12:40');
+INSERT INTO `teams` (`id`, `name`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
+(1, 'Zulu', 1, '1', '2019-08-19 02:12:40', '1', '2019-08-19 02:12:40'),
+(2, 'SPARK', 1, '1', '2019-08-19 02:12:40', '1', '2019-08-19 02:12:40');
 
 -- --------------------------------------------------------
 
@@ -218,14 +220,14 @@ CREATE TABLE `users` (
   `last_modified_by` varchar(255) COLLATE utf8_bin NOT NULL,
   `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_by` varchar(255) COLLATE utf8_bin NOT NULL,
-  `created_by_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `last_login`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_by_time`) VALUES
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `last_login`, `is_active`, `last_modified_by`, `last_modified_time`, `created_by`, `created_time`) VALUES
 (1, 'Super', 'User', 'administrator@csmgroup.com', '$2y$10$e9LFqB0jKJnQnkwxlKWWyuaCU6pA/R0rcyO7mnSPXdqWJV5SHiB0K', NULL, 1, '', '2019-08-19 02:19:39', '', '2019-08-19 02:19:39');
 
 --
