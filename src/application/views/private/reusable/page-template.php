12f60f4349648dd437ed1dfefc6bf58c9a4ccdc5
noahjahn <36347998+noahjahn@users.noreply.github.com>
2019-09-16 17:45:16 -0400

    Moved and renamed the delete modal for asset_types

--- src/application/views/private/reusable/page-template.php
+++ src/application/views/private/reusable/page-template.php
@@ -19,7 +19,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 ?>
             </div>
         </div>
-
-        <?php $this->load->view('private/reusable/confirmation-modal'); ?>
     </body>
 </html>
