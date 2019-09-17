fac1f5f4cafd39f793a10bf2a83b349b9cc9b403
noahjahn <36347998+noahjahn@users.noreply.github.com>
2019-09-16 22:51:37 -0400

    Implemented manufacturers add/edit view

--- src/application/views/private/modals/manufacturers/add-edit.php
+++ src/application/views/private/modals/manufacturers/add-edit.php
@@ -1,20 +1,26 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom-modal.css" />
 
-<div class="modal fade" id="delete-asset-type" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade" id="add-edit-manufacturer" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modal-title-delete-asset-type">Delete Asset Type</h5>
+                <h5 class="modal-title" id="modal-title-add-edit-manufacturer">Manufacturer</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body" id="modal-body-delete-asset-type">
-                Are you sure you want to delete this Asset Type?
-            </div>
-            <div class="modal-footer">
-                <button id="modal-cancel-delete-asset-type" type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button id="modal-confirm-delete-asset-type" type="button" class="btn btn-primary" data-dismiss="modal">Delete</button>
+            <div class="modal-body" id="modal-body-add-edit-manufacturer">
+                <form action="">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="name" class="form-control" id="name">
+                    </div>
+                    <!-- <button type="submit" class="btn btn-primary">Submit</button> -->
+                    <div class="modal-footer">
+                        <button id="modal-cancel-add-edit-manufacturer" type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button id="modal-confirm-add-edit-manufacturer" type="submit" class="btn btn-primary" data-dismiss="modal">Save</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
