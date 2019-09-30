fa321f5ae2f9bfc4c7d69b16684712d32502e439
Paul VanderWeele <faytro@hotmail.com>
2019-09-30 15:49:10 -0400

    formatted table joins for maintainability

--- src/application/models/AssetManager_model.php
+++ src/application/models/AssetManager_model.php
@@ -23,10 +23,14 @@ class AssetManager_model extends CI_Model {
                                 as last_modified_time
             ');
             $this->db->from('assets as a');
-            $this->db->join('asset_types as at', 'a.type = at.id');
-            $this->db->join('teams as t', 'a.team = t.id');
-            $this->db->join('models as mo', 'a.model = mo.id');
-            $this->db->join('manufacturers as ma', 'mo.manufacturer = ma.id');
+            $this->db->join('asset_types as at'
+                          , 'a.type = at.id');
+            $this->db->join('teams as t'
+                          , 'a.team = t.id');
+            $this->db->join('models as mo'
+                          , 'a.model = mo.id');
+            $this->db->join('manufacturers as ma'
+                          , 'mo.manufacturer = ma.id');
             $query = $this->db->get();
 
             return $query;
