cf8dfc5c7272620a51bd8a3af1540d9afdadf870
Noah Jahn <najahn19@gmail.com>
2019-10-21 20:04:27 -0400

    Implemented add, edit, and delete functionality

--- src/application/controllers/Settings.php
+++ src/application/controllers/Settings.php
@@ -45,7 +45,7 @@ class Settings extends CI_Controller {
 	public function asset_manager($data) {
 		$data['data']['data']['asset_types'] = $this->AssetTypes_model->get_active();
 		$data['data']['data']['teams'] = $this->Teams_model->get_active();
-		$data['data']['data']['manufacturers'] = $this->Manufacturers_model->get_active_manufacturers();
+		$data['data']['data']['manufacturers'] = $this->Manufacturers_model->get_active();
 		$data['data']['data']['models'] = $this->Models_model->get_active_models();
 		return $data;
 	}
