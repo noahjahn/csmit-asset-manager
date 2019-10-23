62d16d1a650abf4bc521f4482cacd03f7a62e49c
noahjahn <36347998+noahjahn@users.noreply.github.com>
2019-10-23 19:44:56 -0400

    Add deployer files

---
+++ src/deploy.php
@@ -0,0 +1,24 @@
+<?php
+namespace Deployer;
+
+require 'recipe/codeigniter.php';
+
+// Project name
+set('application', 'csmit');
+
+inventory('hosts.yml');
+set('writable_use_sudo', true);
+
+// Project repository
+set('repository', 'git@github.com:noahjahn/csmit-asset-manager.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+add('shared_dirs', ['uploads']);
+add('writable_dirs', ['uploads']);
+
+set('allow_anonymous_stats', false);
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
