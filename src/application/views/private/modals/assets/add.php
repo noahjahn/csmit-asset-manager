2a0606eb7c972301cb88451819c20c1a7980b79d
Paul VanderWeele <faytro@hotmail.com>
2019-10-28 14:47:26 -0400

    Added a 2nd column to the modal for adding assets.

--- src/application/views/private/modals/assets/add.php
+++ src/application/views/private/modals/assets/add.php
@@ -10,25 +10,57 @@
         </button>
       </div>
       <div class="modal-body">
-        <div class="md-form mb-4">
-          <input type="text" id="input1" class="form-control validate">
-          <label data-success="right" data-error="wrong" for="input1">Input 1</label>
-        </div>
-        <div class="mb-4">
-          <input id="input2" type="text" class="form-control validate">
-          <label data-success="right" data-error="wrong" for="input2">Input 2</label>
-        </div>
-        <div class="mb-4">
-          <input id="input3" type="text" class="form-control validate">
-          <label data-success="right" data-error="wrong" for="input3">Input 3</label>
-        </div>
-        <div class="mb-4">
-          <input id="input4" type="text" class="form-control validate">
-          <label data-success="right" data-error="wrong" for="input4">Input 4</label>
-        </div>
-        <div class="mb-4">
-          <input id="input5" type="text" class="form-control validate">
-          <label data-success="right" data-error="wrong" for="input5">Input 5</label>
+        <div class="md-form container">
+          <div class="row">
+            <div class="col-sm">
+              <input type="text" id="input1-1" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input1-1">Input 1-1</label>
+            </div>
+            <div class="col-sm">
+              <input type="text" id="input1-2" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input1-2">Input 1-2</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm">
+              <input type="text" id="input2-1" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input2-1">Input 2-1</label>
+            </div>
+            <div class="col-sm">
+              <input type="text" id="input2-2" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input2-2">Input 2-2</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm">
+              <input type="text" id="input3-1" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input3-1">Input 3-1</label>
+            </div>
+            <div class="col-sm">
+              <input type="text" id="input3-2" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input3-2">Input 3-2</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm">
+              <input type="text" id="input4-1" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input4-1">Input 4-1</label>
+            </div>
+            <div class="col-sm">
+              <input type="text" id="input4-2" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input4-2">Input 4-2</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm">
+              <input type="text" id="input5-1" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input5-1">Input 5-1</label>
+            </div>
+            <div class="col-sm">
+              <input type="text" id="input5-2" class="form-control validate">
+              <label data-success="right" data-error="wrong" for="input5-2">Input 5-2</label>
+            </div>
+          </div>
         </div>
       </div>
       <div class="modal-footer">
