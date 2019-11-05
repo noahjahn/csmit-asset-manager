0e7b105d4d8d6360116eb363f7409f8ffcdc9b8d
Noah Jahn <najahn19@gmail.com>
2019-11-05 18:13:01 -0500

    Fix responsive headers and data in the data tables

--- src/assets/js/settings.js
+++ src/assets/js/settings.js
@@ -56,6 +56,7 @@ $(function() {
     $('#asset-manager-link').click(function(e) {
         // e.preventDefault();
         makeAssetManagerActive(this);
+        $($.fn.dataTable.tables(true)).DataTable().responsive.recalc().columns.adjust();
     });
 });
 
@@ -87,6 +88,7 @@ $(function() {
             $(this).removeClass('show');
             $(this).removeClass('active');
         });
+        $($.fn.dataTable.tables(true)).DataTable().responsive.recalc().columns.adjust();
     });
 });
 
@@ -118,6 +120,7 @@ $(function() {
             $(this).removeClass('show');
             $(this).removeClass('active');
         });
+        $($.fn.dataTable.tables(true)).DataTable().responsive.recalc().columns.adjust();
     });
 });
 
@@ -149,88 +152,7 @@ $(function() {
             $(this).removeClass('show');
             $(this).removeClass('active');
         });
+        $($.fn.dataTable.tables(true)).DataTable().responsive.recalc().columns.adjust();
     });
 });
 /* *** ************************** *** */
-
-$(document).ready( function () {
-    /* Prepare Models table */
-    // $('#models').DataTable( {
-    //     ajax: {
-    //         url: baseUrl + "Models/get_active",
-    //         dataSrc: ''
-    //     },
-    //     columns: [
-    //         { "data": "id" },
-    //         { "data": "name" },
-    //         { "data": "manufacturer" },
-    //         { "render": function ( data, type, row ) {
-    //                 return '<button class="table-icon" data-toggle="modal" data-target="#add-edit-model" data-type="POST" data-tableid="models" data-url="Models/edit/' + row.id + '" data-target="#add-edit-model"><img class="mini-icon" src="' + baseUrl + 'assets/img/icons/edit-svgrepo-com-white.svg"></button></td>';
-    //             }
-    //         },
-    //         { "render": function ( data, type, row ) {
-    //                 return '<button class="table-icon" data-toggle="modal" data-id="delete-model" data-type="DELETE" data-tableid="models" data-url="Models/delete/' + row.id + '" data-target="#delete-model"><img class="mini-icon" src="' + baseUrl + 'assets/img/icons/trash-can-with-cover-svgrepo-com-white.svg"></button></td>';
-    //             }
-    //         }
-    //     ],
-    //     "order": [[ 1, "asc" ]],
-    //     scrollY:        212,
-    //     paging:         false,
-    //     fixedHeader:    true,
-    //     info:           false,
-    //     columnDefs: [
-    //         { "orderable": false, "targets": [3,4] },
-    //         { "visible": false, "targets": 0 }
-    //     ],
-    //     dom:
-    //         "<'row'<'col-sm'<'table-title-models'>>fB>" +
-	// 		"<'row'<'col-sm'tr>>",
-    //     buttons: [
-    //         {
-    //             text: "Add Model",
-    //             action: function (e, dt, node, config) {
-    //                 // loadManufacturers("model-manufacturer");
-    //             },
-    //             init: function (api, node, config) {
-    //                 $(node).removeClass('btn-secondary');
-    //                 $(node).addClass("add-edit-button");
-    //                 $(node).attr("data-toggle", "modal");
-    //                 $(node).attr("data-url", "Models/add");
-    //                 $(node).attr("data-id", "add-edit-model");
-    //                 $(node).attr("data-type", "POST");
-    //                 $(node).attr("data-tableid", "models");
-    //                 $(node).attr("data-target", "#add-edit-model");
-    //             },
-    //             className: 'btn-primary'
-    //         }
-    //     ],
-    //     language: {
-    //         search: "",
-    //         searchPlaceholder: "Search..."
-    //     }
-    // });
-    // $("div.table-title-models").html('<h5 class="pt-3">Models</h5>');
-    // $("#models_wrapper").addClass("mb-4", "pt-2");
-});
-
-/* *** ************************** *** */
-
-
-// function loadManufacturers(selectId) {
-//     $.ajax({
-//         type: "GET",
-//         url: baseUrl + "Manufacturers/get_active",
-//         data: "{ name: name, id: id }",
-//         contentType: "application/json;",
-//         dataType: "json",
-//         success: function(data)
-//                 {
-//                     $.each(data, function () {
-//                         $("#model-manufacturer").append("<option value=" + this.id + ">" + this.name + "</option>");
-//                     });
-//                 },
-//         failure: function () {
-//             alert("Failed to load manufacturer");
-//         }
-//     });
-// }
