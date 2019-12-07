d9bc3b046a8c6d4feca2cd8c6e518f35bd24876c
Noah Jahn <najahn19@gmail.com>
2019-12-07 09:50:05 -0500

    Add function for checking write access

--- src/application/helpers/authorization_helper.php
+++ src/application/helpers/authorization_helper.php
@@ -17,3 +17,18 @@ if ( ! function_exists('is_authorized')) {
         }
     }
 }
+
+if ( ! function_exists('has_write_access')) {
+    function has_write_access($user_role_id, $page) {
+        $ci =& get_instance();
+        $ci->load->model('Roles_model');
+        switch ($ci->Roles_model->get_page_access($user_role_id, $page)) {
+            case W:
+                return TRUE;
+            case RW:
+                return TRUE;
+            default:
+                return FALSE;
+        }
+    }
+}
