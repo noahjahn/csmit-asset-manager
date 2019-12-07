8491d96af154d31615e16ef9c14267fe04031c27
Noah Jahn <najahn19@gmail.com>
2019-12-07 09:55:14 -0500

    Add user authorization check

--- src/application/controllers/Roles.php
+++ src/application/controllers/Roles.php
@@ -5,23 +5,38 @@ class Roles extends CI_Controller {
 
 	private $user_id;
 	private $user_role_id;
+	private $page;
 
 	public function __construct() {
 		parent::__construct();
-		// check for user authorization
-        $this->load->model('Roles_model');
 		$this->load->helper("database");
 		$this->load->helper("general");
+		$this->load->helper("authorization");
 		$this->user_id = $this->session->userdata('id');
-		$this->user_role_id = $this->session->userdata('role');
-	}
+		$this->user_role_id = $this->session->userdata('id');
+		$this->page = 'roles';
+		if ( ! $this->session->userdata('id')) { // if the user is not logged in
+            redirect('unauthorized');
+		}
+		if ( ! is_authorized($this->user_role_id, $this->page)) {
+			redirect('forbidden');
+		}
 
-	public function index() {
-
+		if ($this->uri->total_segments() > 1) {
+			if ( ! $this->uri->segment(2) == 'get_active') {
+				if ( ! has_write_access($this->user_role_id, $this->page)) {
+					redirect('forbidden');
+				}
+			}
+		}
 	}
 
     public function get_active() {
 		log_message('debug', 'Roles: get_active - in function');
+
+		if (!$this->input->is_ajax_request()) {
+            redirect('forbidden');
+        }
 
 		$active_roles = $this->Roles_model->get_active();
 		$json_roles = json_encode($active_roles);
@@ -34,11 +49,6 @@ class Roles extends CI_Controller {
 		return $this->Roles_model->get_page_access($this->user_role_id, $page);
 	}
 
- 	private function get_dashboard_access() {
-		log_message('debug', 'Roles: get_dashboard_access - in function');
-
-
-    }
 }
 
 ?>
