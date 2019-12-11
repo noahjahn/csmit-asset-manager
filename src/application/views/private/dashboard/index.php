85e255ffaed0ddbf2a9ad048bae3c16ed63905ed
Noah Jahn <najahn19@gmail.com>
2019-12-10 23:58:30 -0500

    Begin dasbhoard design

---
+++ src/application/views/private/dashboard/index.php
@@ -0,0 +1,46 @@
+<div class="row mb-4 dashboard-row">
+    <div class="col">
+        <div class="dasbhoard-box text-center">
+            <div class="dashboard-large-text">
+                test
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="dasbhoard-box text-center">
+            <div class="dashboard-large-text">
+                test
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="dasbhoard-box text-center">
+            <div class="dashboard-large-text">
+                test
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row mb-4 dashboard-row">
+    <div class="col">
+        <div class="dasbhoard-box text-center">
+            <div class="dashboard-large-text">
+
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="dasbhoard-box text-center">
+            <div class="dashboard-large-text">
+
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="dasbhoard-box text-center">
+            <div class="dashboard-large-text">
+
+            </div>
+        </div>
+    </div>
+</div>
