239919c2b10a3c9bae3d4107a45c25ad9df41fd6
Noah Jahn <najahn19@gmail.com>
2020-01-30 20:12:18 -0500

    Remove explicit null declaration

--- src/application/migrations/20200130045220_assets_modify_purchase_date.php
+++ src/application/migrations/20200130045220_assets_modify_purchase_date.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Migration_Assets_modify_purchase_date extends CI_Migration {
     
     public function up() { 
-        $this->dbforge->modify_column('assets', "`purchase_date` DEFAULT NULL");
+        $this->dbforge->modify_column('assets', "`purchase_date`");
     }
     
     public function down() { 
