129951dcda51e6400f53cee925689799bce07859
Noah Jahn <najahn19@gmail.com>
2020-02-01 23:14:13 -0500

    Updates form with new names, aligns all the fields, and adds error validation tags

--- src/application/views/private/modals/assets/add.php
+++ src/application/views/private/modals/assets/add.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom-modal.css"/>
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom-semantic.css"/>
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/libraries/semantic/dropdown.css"/>
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/libraries/semantic/transition.css"/>
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/libraries/semantic/custom.css"/>
@@ -18,63 +19,67 @@
                 <?php echo form_open(current_url(), array('id' => 'add-asset-form', 'name' => 'add-asset-form')); ?>
                     <div class="row">
                         <div class="col-md form-group">
-                            <label for="add-asset-manufacturer">Manufacturer</label>
+                            <label>Manufacturer<span class="required"> *</span></label>
                             <select id="add-asset-manufacturer" name="manufacturer_id" class="form-control ui search dropdown" value="<?php set_value('manufacturer_id'); ?>"></select>
+                            <div id="add-asset-manufacturer-error" class="invalid-feedback"></div>
                         </div>
                         <div class="col-md form-group">
-                            <label for="add-asset-model">Model</label>
+                            <label>Model<span class="required"> *</span></label>
                             <select id="add-asset-model" name="model_id" class="form-control ui search dropdown" value="<?php set_value('model_id'); ?>"></select>
+                            <div id="add-asset-model-error" class="invalid-feedback"></div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md form-group">
-                            <label for="add-asset-type">Type</label>
+                            <label>Type<span class="required"> *</span></label>
                             <select type="text" id="add-asset-type" name="type_id" class="form-control ui search dropdown" value="<?php set_value('type_id'); ?>"></select>
+                            <div id="add-asset-type-error" class="invalid-feedback"></div>
                         </div>
                         <div class="col-md form-group">
-                            <label for="add-asset-type">Rate</label>
+                            <label>Rate<span class="required"> *</span></label>
                             <input type="text" id="add-asset-rate" name="rate" class="form-control" placeholder="$0.00" disabled>
                             <div id="add-asset-rate-error" class="invalid-feedback"></div>
-                        </div>                    
+                        </div>
                     </div>
                     <div class="row">
                         <div class="col-md form-group">
-                            <label for="add-asset-asset-tag">Asset Tag</label>
+                            <label>Asset Tag<span class="required"> *</span></label>
                             <input type="text" id="add-asset-asset-tag" name="asset_tag" class="form-control" value="<?php set_value('asset_tag'); ?>">
                             <div id="add-asset-asset-tag-error" class="invalid-feedback"></div>
                         </div>
                         <div class="col-md form-group">
-                            <label for="add-asset-serial-number">Serial Number</label>
+                            <label>Serial Number</label>
                             <input type="text" id="add-asset-serial-number" name="serial_number" class="form-control" value="<?php set_value('serial_number'); ?>">
                             <div id="add-asset-serial-number-error" class="invalid-feedback"></div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md form-group">
-                            <label for="add-asset-team">Team</label>
+                            <label>Team<span class="required"> *</span></label>
                             <select type="text" id="add-asset-team" name="team_id" class="form-control ui search dropdown" value="<?php set_value('team_id'); ?>"></select>
+                            <div id="add-asset-team-error" class="invalid-feedback"></div>
                         </div>
                         <div class="col-md form-group">
-                            <label for="add-asset-owner">Owner</label>
+                            <label>Owner<span class="required"> *</span></label>
                             <input id="add-asset-owner" name="owner" class="form-control" value="<?php set_value('owner'); ?>">
                             <div id="add-asset-owner-error" class="invalid-feedback"></div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md form-group">
-                            <label for="add-asset-location">Location</label>
+                            <label>Location</label>
                             <input type="text" id="add-asset-location" name="location" class="form-control" value="<?php set_value('location'); ?>">
                             <div id="add-asset-location-error" class="invalid-feedback"></div>
                         </div>
                         <div class="col-md form-group">
-                            <label for="add-asset-job-number">Job Number</label>
+                            <label>Job Number</label>
                             <input type="text" id="add-asset-job-number" name="job_number" class="form-control" value="<?php set_value('job_number'); ?>">
                             <div id="add-asset-job-number-error" class="invalid-feedback"></div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md form-group">
-                            <label for="add-asset-purchase-date">Purchase Date</label>
+                            <label>Purchase Date</label>
                             <input type="date" id="add-asset-purchase-date" name="purchase_date" class="form-control" value="<?php set_value('purchase_date'); ?>">
                             <div id="add-asset-purchase-date-error" class="invalid-feedback"></div>
                         </div>
@@ -88,7 +93,7 @@
                         <div class="col-md form-group no-width">
                             <label for="add-asset-notes">Notes</label>
                             <textarea class="form-control textarea" id="add-asset-notes" name="notes" rows="2" value="<?php set_value('notes'); ?>"></textarea>
-                            <div id="add-asset-notess=-error" class="invalid-feedback"></div>
+                            <div id="add-asset-notes-error" class="invalid-feedback"></div>
                         </div>
                     </div>
                     <div>
