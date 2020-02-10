659cf48649dc44c6684357697830fa3d115770b2
Noah Jahn <najahn19@gmail.com>
2020-02-09 20:56:04 -0500

    Implements updating an asset and edit field rules

--- src/application/models/AssetManager_model.php
+++ src/application/models/AssetManager_model.php
@@ -174,6 +174,51 @@ class AssetManager_model extends CI_Model {
         return $notes_rules;
     }
 
+    function get_update_rules() {
+        log_message('debug', 'AssetManager_model: get_update_rules - in function');
+
+        $form_rules = array(
+            $this->get_insert_manufacturer_id_rules(),
+            $this->get_insert_model_id_rules(),
+            $this->get_insert_owner_rules(),
+            $this->get_update_serial_number_rules(),
+            $this->get_insert_type_id_rules(),
+            $this->get_update_asset_tag_rules(),
+            $this->get_insert_team_id_rules(),
+            $this->get_insert_purchase_price_rules(),
+            $this->get_insert_purchase_date_rules(),
+            $this->get_insert_job_number_rules(),
+            $this->get_insert_location_rules(),
+        );
+        return $form_rules;
+    }
+
+    function get_update_serial_number_rules() {
+        log_message('debug', 'AssetManagers_model: get_update_serial_number_rules - in function');
+        $serial_number_rules = array(
+            'field' => $this->fields['serial_number'],
+            'label' => 'serial number',
+            'rules' => 'trim|callback_is_serial_number_unique_not_different_from_current',
+            'errors' => array (
+                'is_serial_number_unique_not_different_from_current' => 'The %s field must contain a unique value.'
+            )
+        );
+        return $serial_number_rules;
+    }
+
+    function get_update_asset_tag_rules() {
+        log_message('debug', 'AssetManagers_model: get_update_asset_tag_rules - in function');
+        $asset_tag_rules = array(
+            'field' => $this->fields['asset_tag'],
+            'label' => 'asset tag',
+            'rules' => 'required|callback_is_asset_tag_unique_not_different_from_current|trim',
+            'errors' => array (
+                'is_asset_tag_unique_not_different_from_current' => 'The %s field must contain a unique value.'
+            )
+        );
+        return $asset_tag_rules;
+    }
+
     public function get_active() {
         $this->db->select('
           a.id              as id,
@@ -219,9 +264,28 @@ class AssetManager_model extends CI_Model {
         $this->db->insert($this->table, $asset);
     }
 
-    function update_asset() {
+    function update($asset) {
+        log_message('debug', 'AssetManager_model: update - in function');
 
-
+        // check if asset passed in exists and is active
+        if (record_exists($asset['id'], $this->table) && !(record_is_deleted($asset['id'], $this->table))) {
+            // if it is, update it
+            if ($this->is_asset_tag_unique_not_different_from_current($asset['asset_tag'], $asset['id'])) {
+                if ($this->is_serial_number_unique_not_different_from_current($asset['serial_number'], $asset['id'])) {
+                    $this->db->where('id', $asset['id']);
+                    $this->db->update($this->table, $asset);
+                } else {
+                    log_message('error', 'AssetManager_model: update - failed, record '.$asset['serial_number'].' isn\'t unique');
+                    return FALSE;
+                }
+            } else {
+                log_message('error', 'AssetManager_model: update - failed, record '.$asset['asset_tag'].' isn\'t unique');
+                return FALSE;
+            }
+        } else {
+            log_message('error', 'AssetManager_model: update - failed, record '.$asset['id'].' doesn\'t exist or is inactive');
+            return FALSE;
+        }
     }
 
     function delete($id) {
@@ -299,8 +363,63 @@ class AssetManager_model extends CI_Model {
             log_message('debug', 'AssetManager_model: is_asset_tag_unique - found more than one record with the same asset tag '.$asset_tag);
             return FALSE;
         }
-
     }
+
+    function is_serial_number_unique_not_different_from_current($serial_number, $id) {
+        log_message('debug', 'AssetManager_model: is_serial_number_unique_not_different_from_current - in function');
+        if (isset($serial_number) && $serial_number != '') {
+            log_message('debug', $serial_number);
+            $this->db->select('*');
+            $this->db->from($this->table);
+            $this->db->where('serial_number', $serial_number);
+            $this->db->where('is_deleted', 0);
+
+            $query = $this->db->get();
+
+            if ($query->num_rows() == 1) {
+                if ($query->result_array()[0]['id'] == $id) {
+                    return TRUE;
+                } else {
+                    log_message('error', 'AssetManager_model: is_serial_number_unique_not_different_from_current - the serial number '.$serial_number.' entered already exists');
+                    return FALSE;
+                }
+            } else if ($query->num_rows() == 0) {
+                log_message('debug', 'AssetManager_model: is_serial_number_unique_not_different_from_current - a new serial number has been entered: '.$serial_number);
+                return TRUE;
+            } else {
+                log_message('error', 'AssetManager_model: is_serial_number_unique_not_different_from_current - found more than one record with the same serial number: '.$serial_number);
+                return FALSE;
+            }
+        } else {
+            return TRUE;
+        }
+	}
+
+    function is_asset_tag_unique_not_different_from_current($asset_tag, $id) {
+        log_message('debug', 'AssetManager_model: is_asset_tag_unique_not_different_from_current - in function');
+
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('asset_tag', $asset_tag);
+        $this->db->where('is_deleted', 0);
+
+        $query = $this->db->get();
+
+        if ($query->num_rows() == 1) {
+            if ($query->result_array()[0]['id'] == $id) {
+                return TRUE;
+            } else {
+                log_message('error', 'AssetManager_model: is_asset_tag_unique_not_different_from_current - the asset tag '.$asset_tag.' entered already exists');
+                return FALSE;
+            }
+        } else if ($query->num_rows() == 0) {
+            log_message('debug', 'AssetManager_model: is_asset_tag_unique_not_different_from_current - a new asset tag has been entered: '.$asset_tag);
+            return TRUE;
+        } else {
+            log_message('error', 'AssetManager_model: is_asset_tag_unique_not_different_from_current - found more than one record with the same asset tag: '.$asset_tag);
+            return FALSE;
+        }
+	}
 }
 
 ?>
