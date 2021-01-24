d08096f81020a6df01ff8c524204bd27af1cabdf
Noah Jahn <najahn19@gmail.com>
2021-01-23 23:02:19 -0500

    Added function to set random chart colors

--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -1,3 +1,20 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function getChartBackgroundColors(data) {
+    const colors = [];
+    for (let i = 0; i < data.length; i += 1) {
+        colors.push(getRandomColor());
+    }
+    return colors;
+}
+
 $(document).ready(function() {
     var assetTypeLabels = [];
     assetTypeLabels = get_asset_type_labels();
